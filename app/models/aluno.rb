d2c3f99aaecfa596679e54713199402fedcb81aa
gabrielsleal <sgabrielleal@gmail.com>
2018-03-08 23:30:56 -0300

    Terminado cadastro funcional de alunos

--- app/models/aluno.rb
+++ app/models/aluno.rb
@@ -1,2 +1,24 @@
 class Aluno < ApplicationRecord
+
+    validates :nome,:codigo,:data_nascimento,:nome_mae, :situacao, presence:true
+    validates :codigo,:email, uniqueness:true
+
+    def sexo_enum
+        ['Masculino','Feminino']
+    end
+
+    def situacao_enum
+        ['Ativo','Inativo','Em débito']
+    end
+
+    def formacao_enum
+        [
+            'Ensino básico incompleto',
+            'Ensino básico completo',
+            'Ensino médio incompleto',
+            'Ensino médio completo',
+            'Ensino superior incompleto',
+            'Ensino superior completo'
+        ]
+    end
 end
